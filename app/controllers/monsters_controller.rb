e58b4871e64ee4feb74da0628d3c35b0db5366eb
Nyoho~ <luciocanessa@gmail.com>
2022-10-18 20:20:48 +0000

    Hay que arreglar la wea del permit con el formulario

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -6,4 +6,20 @@ class MonstersController < ApplicationController
   def show
     @monster = Monster.find(params[:id])
   end
+
+  def create
+    @monster = Monster.new(monster_params)
+    if @monster.save
+        redirect_to @monster, notice: "Se ha creado el monstruo correctamente"
+    else
+        render :new
+    end
+
+  private
+  
+  def monster_params
+    params.permit(:name, :decription, :phone, :birthdate) #Preguntar el por que funciona con require
+  end
+
+end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :monsters, only: [:index]
+  resources :monsters, only: [:index, :new, :edit, :update]
+
+  post '/monsters/new', to: 'monsters#create'
+  get '/monsters/:id/edit', to: 'monsters#edit'
+  put '/monsters/:id', to: 'monsters#update'
 
   root "monsters#index"
 end
